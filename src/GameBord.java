4aabe3d2480dafd46f905b19bedcdfc51a41dce2
Pixeldeck <philipmir@hotmail.com>
2022-10-17 15:59:25 +0200

    Lite kod 1

--- src/GameBord.java
+++ src/GameBord.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 public class GameBord {
+
+    static ArrayList playerXPos = new ArrayList<>(); // Static ArrayList that can use Int
+    static ArrayList PlayerOPos = new ArrayList<>();
     private String[][] GameTable;
 
     public GameBord() {
@@ -14,10 +19,7 @@ public class GameBord {
     }
 
 
-    public String[][] getGameTable() {
 
-        return GameTable;
-    }
 
 
     public void print() {
@@ -30,5 +32,51 @@ public class GameBord {
         System.out.println("  ---+---+---+---+---");
         System.out.println("d "+GameTable[3][0] + "|"+ GameTable[3][1] + "|"+ GameTable[3][2] + "|"+ GameTable[3][3] + "|"+ GameTable[3][4]);
 
+    }
+
+    public static void playerSymbol(String [][] GameBoard , String squarePosition, String user) {
+
+
+        String symbol = "";     // Empty char for use in gameBoard that is char based.
+
+        if (user.equals("PlayerX")) {
+            symbol = "X";
+            playerXPos.add(squarePosition);  // symbol get placed is the switch -> gameBoard depending on what number user press.
+        } else if (user.equals("PlayerO")) {
+            symbol = "O";
+            PlayerOPos.add(squarePosition);
+        }
+
+        switch (squarePosition) {    // 2D Array positions via a switch.
+            case "1":
+                GameBoard[0][1] = symbol;    // Symbol = X or O depending on what player picking a spot.
+                break;
+            case "2":                          // [0][0] = first column fist row.
+                GameBoard[0][3] = symbol;    // [0][1] = second column first row. jumping over char  '|' in gameBoard
+                break;
+            case "3":
+                GameBoard[0][5] = symbol;
+                break;
+            case "4":
+                GameBoard[2][1] = symbol;
+                break;
+            case "5":
+                GameBoard[2][3] = symbol;
+                break;
+            case "6":
+                GameBoard[2][5] = symbol;
+                break;
+            case "7":
+                GameBoard[4][1] = symbol;
+                break;
+            case "8":
+                GameBoard[4][3] = symbol;
+                break;
+            case "9":
+                GameBoard[4][5] = symbol;
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.*;  // util wildcard (random)
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         GameBord g1 = new GameBord();
-        g1.print();
+
+       g1.print();
+
+        System.out.println("PlayerX");
+        String player = scan.nextLine();
+
+        g1.playerSymbol( "1", "PlayerX");
     }
 }
